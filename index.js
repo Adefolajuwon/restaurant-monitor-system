03a23a41c87b5d633886d9d8bc324b2fb70c6c13
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-01-03 03:34:17 +0100

    image process

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-import dotenv from "dotenv";
-import expressService from "./services/express.service.js";
-import sequelizeService from "./services/sequelize.service.js";
-dotenv.config();
+import http from 'http';
+import express from 'express';
+import sequelizeService from './services/sequelize.service.js';
 
-const services = [expressService, sequelizeService];
+const PORT = process.env.PORT || 3005;
 
-(async () => {
-  try {
-    for (const service of services) {
-      await service.init();
-    }
-    console.log("Server initialized.");
-    //PUT ADITIONAL CODE HERE.
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-})();
+const serverApp = express();
+serverApp.set('trust proxy', true);
+
+// Middleware to set content type
+serverApp.use((req, res, next) => {
+	res.setHeader('Content-Type', 'application/json');
+	next();
+});
+
+// Middleware to parse JSON requests
+serverApp.use(express.json());
+
+// Error handling middleware
+serverApp.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Something went wrong!');
+});
+
+const server = http.createServer(serverApp);
+
+// Initialize Sequelize and start server
+sequelizeService.init().then(async () => {
+	server.listen(PORT, () => {
+		console.log(`Server started on PORT ${PORT}...`);
+	});
+});
--- services/sequelize.service.js
+++ services/sequelize.service.js
@@ -1,36 +1,60 @@
-import { Sequelize } from "sequelize";
-import datbaseConfig from '../config/database.js'
-import fs from "fs";
+import { Sequelize } from 'sequelize';
+import databaseConfig from '../config/database.js';
 
-const modelFiles = fs
-  .readdirSync(__dirname + "/../models/")
-  .filter((file) => file.endsWith(".js"));
+import { readdirSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// Get the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const modelsPath = join(__dirname, '/../models/');
+
+// Now you can use readdirSync with the correct path
+const files = readdirSync(modelsPath);
 
 const sequelizeService = {
-  init: async () => {
-    try {
-      let connection = new Sequelize(databaseConfig);
+	init: async () => {
+		try {
+			let connection = new Sequelize(databaseConfig);
 
-      /*
+			/*
         Loading models automatically
       */
-     
-      for (const file of modelFiles) {
-        const model = await import(`../models/${file}`);
-        model.default.init(connection);
-      }
 
-      modelFiles.map(async (file) => {
-        const model = await import(`../models/${file}`);
-        model.default.associate && model.default.associate(connection.models);
-      });
+			for (const file of files) {
+				const modelModule = await import(`../models/${file}`);
+				const model = modelModule.default;
 
-      console.log("[SEQUELIZE] Database service initialized");
-    } catch (error) {
-      console.log("[SEQUELIZE] Error during database service initialization");
-      throw error;
-    }
-  },
+				if (model && typeof model.init === 'function') {
+					model.init(connection);
+				} else {
+					console.warn(
+						`[SEQUELIZE] Skipping model initialization for ${file}. Missing init method.`
+					);
+				}
+			}
+
+			files.map(async (file) => {
+				const modelModule = await import(`../models/${file}`);
+				const model = modelModule.default;
+
+				if (model && typeof model.associate === 'function') {
+					model.associate(connection.models);
+				} else {
+					console.warn(
+						`[SEQUELIZE] Skipping model association for ${file}. Missing associate method.`
+					);
+				}
+			});
+
+			console.log('[SEQUELIZE] Database service initialized');
+		} catch (error) {
+			console.error('[SEQUELIZE] Error during database service initialization');
+			throw error;
+		}
+	},
 };
 
 export default sequelizeService;
