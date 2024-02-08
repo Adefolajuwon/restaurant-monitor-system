aa464c590b5206dc49cbb8464cf9c044de33775a
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-02-08 06:22:42 +0100

    uptime_last_hour

--- helpers/time.js
+++ helpers/time.js
@@ -2,11 +2,28 @@ import { db } from '../services/knex.services.js';
 import { DateTime } from 'luxon';
 
 const dateTime = DateTime.now();
-export const uptime_last_hour = async () => {
+export const uptime_last_hour = async (store_id) => {
 	try {
-	} catch (error) {}
+		let storeArray;
+		const timestamps = [];
+		storeArray = await db('Store').where('store_id', '=', store_id);
+		if (!storeArray || storeArray.length === 0) {
+			console.log('No store found with the provided store_id');
+			return;
+		}
+
+		const timezone = await getTimeZone(store_id);
+
+		for (let i = 0; i < storeArray.length; i++) {
+			const obj = storeArray[i];
+			timestamps.push(obj.timestamp_utc);
+		}
+	} catch (error) {
+		console.error('Error querying the database:', error);
+	}
 };
 
+/**Get the store timezon and if data is missing for the store set the timezone to 'america/chicago' */
 const getTimeZone = async (store_id) => {
 	try {
 		let timezone;
@@ -16,8 +33,9 @@ const getTimeZone = async (store_id) => {
 				timezone_str: dateTime.setZone('America/Chicago'),
 			};
 		}
-		console.log(timezone);
+		// console.log(timezone);
 	} catch (error) {
 		console.error('Error fetching timezone:', error);
 	}
 };
+uptime_last_hour('299331931566263572');
